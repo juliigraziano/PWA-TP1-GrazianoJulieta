dbcdcc83b79b1abde272a4e46d8a3f9d2d6f4208
juliigraziano <65053905+juliigraziano@users.noreply.github.com>
2021-12-03 17:01:00 -0300

    LOCAL STORAGE

--- js/clima.js
+++ js/clima.js
@@ -1,4 +1,5 @@
 //Ciudad actual var global
+var map='';
 const value_busqueda = document.getElementById('busqueda');
 
 document.body.addEventListener('submit', ()=>{
@@ -6,9 +7,28 @@ document.body.addEventListener('submit', ()=>{
     obtenerDatos(value_busqueda.value);
 }); 
 
+window.addEventListener('load', imprimir(obtenerDatosLS()));
 
+//---------------------LOCAL STORAGE--------------------//
+
+function obtenerDatosLS(){
+    let datos = JSON.parse(localStorage.getItem('datosUltCiudad'));
+    if (datos != null){
+        
+        createMap(datos.coord.lat, datos.coord.lon);
+  
+    } else {
+
+        alert('Busca una ciudad en el buscador del menu para comenzar!');
+    
+        createMap(0,0,0);
+    }
+   return datos;
+}
+
+
+//--------------------OBTENER DATOS---------------------//
 function obtenerDatos(ciudadActual){
-    console.log('diste click');
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudadActual}&appid=cba22844979badce462750c91e19f19d`;
 
     const api = new XMLHttpRequest();
@@ -19,82 +39,92 @@ function obtenerDatos(ciudadActual){
     api.onreadystatechange = function(){
         if(this.status == 200 && this.readyState == 4){
             let datos = JSON.parse(this.responseText);
+            console.log(datos);
 
-            //Agarro lo que está dentro de Main -temperatura, st, presión
-            let itemMain = datos.main;
-
-            let weatherObeject = {
-                 img: `http://openweathermap.org/img/wn/${datos.weather[0].icon}@2x.png`,
-                 clima : datos.weather[0].main,
-                 climaDescripcion: datos.weather[0].description,
-                 sensacionT : toCelsius(itemMain.feels_like)+'°',
-                 humedad : itemMain.humidity+'%',
-                 presion : itemMain.pressure+'hPa',
-                 temperatura : toCelsius(itemMain.temp)+'°',
-                 maxima : toCelsius(itemMain.temp_max)+'°',
-                 minima : toCelsius(itemMain.temp_min)+'°',
-                 viento : datos.wind.speed+'m/s',
-            };
-//----------Cambia color de display según temperatura---------//
-            colorDisplay(itemMain.temp);
-
-//----------Cambia fondo del main según clima-----------------//
-           fondoMain(weatherObeject.clima);
-         console.log(weatherObeject.clima);
-
-
-//-------------IMPRIMIR DATOS----------------------------------//
-            let display = document.getElementById('resultadoDisplay');
-            let detallado = document.getElementById('resultadoDetallado');
-            document.getElementById('city').innerHTML=`${ciudadActual}`;
+            //localstorage
+           localStorage.setItem('datosUltCiudad', JSON.stringify(datos));
             
-            display.innerHTML=`
-                <img src="${weatherObeject.img}" alt="${weatherObeject.clima}">
-                
-                <div class="row align-items-center justify-content-center">
-                    <span class="col-3">${weatherObeject.temperatura}</span>
+            imprimir(datos);
 
-                    <div class="col-3">
-                    <span>${weatherObeject.maxima}</span>
-                    <span>| ${weatherObeject.minima}</span>
-                    </div>
-                    
-                </div>
-                    <p>${ciudadActual}</p>
-                    <p>${weatherObeject.climaDescripcion}</p>
-                    <p><abbr title="Sensación Térmica">ST</abbr>${weatherObeject.sensacionT}</p>`;
-
-            detallado.innerHTML=`
-                <div class="row row-cols-2 row-cols-md-4 justify-content-evenly ">
-
-                <div class="display col-4 m-2 col-md-2 pt-3">
-                    <p><abbr title="Sensación Térmica">ST</abbr></p>
-                    <p>${weatherObeject.sensacionT}</p>
-                </div>
-                
-                <div class="display col-4 m-2 col-md-2 pt-3">
-                <p>Humedad</p>
-                <p>${weatherObeject.humedad}</p>
-                </div>
-
-                <div class="display col-4 m-2 col-md-2 pt-3">
-                    <p>Viento</p>
-                    <p>${weatherObeject.viento}</p>
-                </div>
-
-                <div class="display col-4 m-2 col-md-2 pt-3">
-                    <p>Presión</p>
-                    <p>${weatherObeject.presion}</p>
-                </div>
-                </div>`;
-
-                console.log(datos.coord);
-
-               updateMap(datos.coord.lat, datos.coord.lon);
-        }
+           }
     }
 
 }
+
+/*----------IMPRIMIR------------*/
+
+function imprimir (datos){
+     //Agarro lo que está dentro de Main -temperatura, st, presión
+     let itemMain = datos.main;
+
+     let weatherObeject = {
+          ciudad: datos.name,
+          img: `http://openweathermap.org/img/wn/${datos.weather[0].icon}@2x.png`,
+          clima : datos.weather[0].main,
+          climaDescripcion: datos.weather[0].description,
+          sensacionT : toCelsius(itemMain.feels_like)+'°',
+          humedad : itemMain.humidity+'%',
+          presion : itemMain.pressure+'hPa',
+          temperatura : toCelsius(itemMain.temp)+'°',
+          maxima : toCelsius(itemMain.temp_max)+'°',
+          minima : toCelsius(itemMain.temp_min)+'°',
+          viento : datos.wind.speed+'m/s',
+     };
+    //Cambia color de display según temperatura//
+     colorDisplay(itemMain.temp);
+
+//----------Cambia fondo del main según clima-----------------//
+     fondoMain(weatherObeject.clima);
+
+//-------------IMPRIMIR DATOS----------------------------------//
+     let display = document.getElementById('resultadoDisplay');
+     let detallado = document.getElementById('resultadoDetallado');
+     document.getElementById('city').innerHTML=`${weatherObeject.ciudad}`;
+     
+     display.innerHTML=`
+         <img src="${weatherObeject.img}" alt="${weatherObeject.clima}">
+         
+         <div class="row align-items-center justify-content-center">
+             <span class="col-3">${weatherObeject.temperatura}</span>
+
+             <div class="col-3">
+             <span>${weatherObeject.maxima}</span>
+             <span>| ${weatherObeject.minima}</span>
+             </div>
+             
+         </div>
+             <p>${weatherObeject.ciudad}</p>
+             <p>${weatherObeject.climaDescripcion}</p>
+             <p><abbr title="Sensación Térmica">ST</abbr>${weatherObeject.sensacionT}</p>`;
+
+     detallado.innerHTML=`
+         <div class="row row-cols-2 row-cols-md-4 justify-content-evenly ">
+
+         <div class="display col-4 m-2 col-md-2 pt-3">
+             <p><abbr title="Sensación Térmica">ST</abbr></p>
+             <p>${weatherObeject.sensacionT}</p>
+         </div>
+         
+         <div class="display col-4 m-2 col-md-2 pt-3">
+         <p>Humedad</p>
+         <p>${weatherObeject.humedad}</p>
+         </div>
+
+         <div class="display col-4 m-2 col-md-2 pt-3">
+             <p>Viento</p>
+             <p>${weatherObeject.viento}</p>
+         </div>
+
+         <div class="display col-4 m-2 col-md-2 pt-3">
+             <p>Presión</p>
+             <p>${weatherObeject.presion}</p>
+         </div>
+         </div>`;
+
+        updateMap(datos.coord.lat, datos.coord.lon);
+ 
+}
+
 
 /*-------------CAMBIO DE COLOR X TEMPERATURA----------*/
 var lastClass='standar';
@@ -159,15 +189,18 @@ function fondoMain(clima){
     fondo.classList.add(lastFondo);
 }
 
+/*------------TOMTOM MAP--------------------*/
 
-//TOMTOM MAP
-
-var map = tt.map({
-    key: 'Vcoh38yYkRiGbBWtUlqfv07yA0A61uHo',
-    container: 'map',
-});
-map.addControl(new tt.FullscreenControl());
-map.addControl(new tt.NavigationControl());
+function createMap(lat,lon, zoomValue=8){
+        map = tt.map({
+        key: 'Vcoh38yYkRiGbBWtUlqfv07yA0A61uHo',
+        container: 'map',
+        center: [lon,lat],
+        zoom: zoomValue,
+    });
+    map.addControl(new tt.FullscreenControl());
+    map.addControl(new tt.NavigationControl());   
+}
 
 function updateMap(locationLat,locationLong){
     map.flyTo({
